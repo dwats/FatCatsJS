69b8b3503150e8ef3d3d1e6e04b4f393440470e3
Eric Julius <ejulius86@gmail.com>
2016-03-18 18:19:03 -0500

    Cleaned up var names.

--- bin/fat-cats.js
+++ bin/fat-cats.js
@@ -1,6 +1,5 @@
 'use strict';
 var DashWatch = require('../lib/dash-arp-listener.js');
-
 var sheetkey = process.env.SHEETKEY;
 var dashmac = process.env.DASHMAC;
 var googauth = require('../fat-cats-558f85c4f2d6.json');
