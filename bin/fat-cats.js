802c31fe0a92573144db9c7f4eefc77abb18ac28
Eric Julius <ejulius86@gmail.com>
2016-03-18 12:18:09 -0500

    Trying some things. Object constructor not working across files.

--- bin/fat-cats.js
+++ bin/fat-cats.js
@@ -2,13 +2,13 @@
 var dash_sniffer = require('../lib/dash-arp-sniffer.js');
 
 var sheetkey = process.env.SHEETKEY;
-var googauth = require('../fat-cats-558f85c4f2d6.json');
 var dashmac = process.env.DASHMAC;
+var googauth = require('../fat-cats-558f85c4f2d6.json');
 
 var dash_sniffer = new dash_sniffer({
   sheetkey: sheetkey,
   googauth: googauth,
   dashmac: dashmac
-})
+});
 
 dash_sniffer.start();
