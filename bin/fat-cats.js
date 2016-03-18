c58a4ec04d9c64c0b0bca01bc5547aaa49025c3f
Eric Julius <ejulius86@gmail.com>
2016-03-18 01:31:34 -0500

    Not working, but done for the night.

--- bin/fat-cats.js
+++ bin/fat-cats.js
@@ -1,2 +1,14 @@
-var spreadsheet-key = process.env.goog_sheet_key;
-var cred = require('./fat-cats-558f85c4f2d6.json');
+'use strict';
+var dash_sniffer = require('../lib/dash-arp-sniffer.js');
+
+var sheetkey = process.env.SHEETKEY;
+var googauth = require('../fat-cats-558f85c4f2d6.json');
+var dashmac = process.env.DASHMAC;
+
+var dash_sniffer = new dash_sniffer({
+  sheetkey: sheetkey,
+  googauth: googauth,
+  dashmac: dashmac
+})
+
+dash_sniffer.start();
