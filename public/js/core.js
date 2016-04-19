e4efed3cbcf9ec94bbfe7567eb49bfd56690cc8a
Eric Julius <ejulius86@gmail.com>
2016-04-19 05:33:19 -0500

    Added catfood api data to . Added elapsed time function, found at https://gist.github.com/lukevella

--- public/js/core.js
+++ public/js/core.js
@@ -4,9 +4,19 @@ var fatCatsFeeding = angular.module('fatCatsFeeding', []);
 function mainController($scope, $http) {
   $scope.formData = {};
   $scope.quantity = 5;
+
+
   $http.get('/api/feedings')
     .success(function(data) {
       $scope.feedings = data;
+    })
+    .error(function(data) {
+      console.log('ERR: '+data);
+    });
+
+  $http.get('/api/catfood')
+    .success(function(data) {
+      $scope.catfoods = data;
     })
     .error(function(data) {
       console.log('ERR: '+data);
@@ -33,3 +43,31 @@ function mainController($scope, $http) {
       });
   };
 }
+
+fatCatsFeeding.filter('elapsed', function(){
+  return function(date){
+      if (!date) return;
+      var time = Date.parse(date),
+          timeNow = new Date().getTime(),
+          difference = timeNow - time,
+          seconds = Math.floor(difference / 1000),
+          minutes = Math.floor(seconds / 60),
+          hours = Math.floor(minutes / 60),
+          days = Math.floor(hours / 24);
+      if (days > 1) {
+          return days + " days ago";
+      } else if (days == 1) {
+          return "1 day ago";
+      } else if (hours > 1) {
+          return hours + " hours ago";
+      } else if (hours == 1) {
+          return "an hour ago";
+      } else if (minutes > 1) {
+          return minutes + " minutes ago";
+      } else if (minutes == 1){
+          return "a minute ago";
+      } else {
+          return "a few seconds ago";
+      }
+  };
+});
