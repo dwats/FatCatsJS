d7f1a95bdc9b99d5530043070e8876976a342d89
Eric Julius <ejulius86@gmail.com>
2016-04-21 00:08:11 -0500

    Added http.put scope function. Added scope.amounts for static dropdown values. Encapsulated http.gets in .refresh.

--- public/js/core.js
+++ public/js/core.js
@@ -1,31 +1,35 @@
 // public/core.js
 var fatCatsFeeding = angular.module('fatCatsFeeding', []);
 
-fatCatsFeeding.controller('Controller', ['$scope', '$http', function($scope, $http) {
+fatCatsFeeding.controller('Controller', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
   $scope.formData = {};
   $scope.quantity = 5;
+  $scope.feedDate = '';
+  $scope.amounts = [{"id": "1/2"}, {"id":"1/3"}, {"id":"1"}];
+  $scope.refresh = function() {
+    /**
+     * Set $scope.feedings to feedings API get results.
+     */
+    $http.get('/api/feedings')
+      .success(function(data) {
+        $scope.feedings = data;
+      })
+      .error(function(data) {
+        console.log('ERR: '+data);
+      });
 
-  /**
-   * Set $scope.feedings to feedings API get results.
-   */
-  $http.get('/api/feedings')
-    .success(function(data) {
-      $scope.feedings = data;
-    })
-    .error(function(data) {
-      console.log('ERR: '+data);
-    });
-
-  /**
-   * Set $scope.catfoods to catfoods API get results.
-   */
-  $http.get('/api/catfood')
-    .success(function(data) {
-      $scope.catfoods = data;
-    })
-    .error(function(data) {
-      console.log('ERR: '+data);
-    });
+    /**
+     * Set $scope.catfoods to catfoods API get results.
+     */
+    $http.get('/api/catfoods')
+      .success(function(data) {
+        $scope.catfoods = data;
+      })
+      .error(function(data) {
+        console.log('ERR: '+data);
+      });
+  };
+  $scope.refresh();
 
   /**
    * POST form results to /api/feedings
@@ -40,20 +44,63 @@ fatCatsFeeding.controller('Controller', ['$scope', '$http', function($scope, $ht
         console.log('ERR: '+data);
       });
   };
-
   /**
    * DELETE by ID
    */
   $scope.deleteFeeding = function(id) {
     $http.delete('/api/feedings/'+id)
       .success(function(data) {
-        $scope.feedings = data;
+        $scope.refresh();
       })
       .error(function(data) {
           console.log('ERR: '+data);
       });
   };
+  /**
+   * Remove with undo
+   */
+   var deferId; //interval id
+   $scope.showUndo = false; // Hide undo warning
+   $scope.removeFeeding = function(id, date) {
+     $scope.feedDate = new Date(date);
+     $scope.showUndo = true; // Show undo warning
+     $scope.feedings = _.filter($scope.feedings, function(feeding) {
+       return feeding._id !== id;
+     });
+     deferId = $interval(function () {
+          $scope.showUndo = false;
+          $scope.deleteFeeding(id);
+          $interval.cancel(deferId);
+      }, 15000);
+   };
+
+ /**
+  * Undo Remove
+  */
+  $scope.undoRemove = function() {
+      $interval.cancel(deferId);
+      $scope.showUndo = false;
+      $scope.refresh();
+  };
+
+ /**
+  * PUT by ID
+  */
+  $scope.update = function(feeding, food, foodAmount) {
+    console.log(feeding);
+    console.log(food);
+    console.log(foodAmount);
+    $http.put('/api/feedings/'+feeding, {food: food, amount: foodAmount})
+      .success(function(data) {
+        $scope.refresh();
+      })
+      .error(function(data) {
+          console.log('ERR: '+data);
+      });
+  };
+
 }]);
+
 
 fatCatsFeeding.directive('myElapsed', ['$interval', function($interval) {
   function link(scope, element, attr) {
