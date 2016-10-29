0d8b5e9e00c504aed940a9243f6afbf04e22b7e5
Eric Julius <ejulius86@gmail.com>
2016-10-29 00:29:25 -0500

    rough testing file for lib/server.class.js

--- test/test-express.js
+++ test/test-express.js
@@ -7,22 +7,3 @@ const httpServer = new HttpServer({
 });
 
 httpServer.run();
-
-const options = {
-  url: 'http://192.168.1.18:8080/api/litter',
-  form: {
-    datetime: new Date()
-  }
-};
-
-request.post(options, (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
-
-request('http://localhost:8080/api/litters', (err, res, body) => {
-  console.log(body);
-});
-
-request('http://192.168.1.18:8080/api/quit');
