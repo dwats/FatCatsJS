6f2f4d00f3f21f0a408e08ff220213d8e2a27b1c
Eric Julius <ejulius86@gmail.com>
2017-02-14 03:04:22 -0600

    Now pulling data from API endpoints and generating cards per endpoint.

--- public/js/core.js
+++ public/js/core.js
@@ -1,23 +1,30 @@
-angular.module('reminderApp', ['ngRoute', 'ngResource'])
+angular.module('reminderApp', ['ngRoute', 'ngResource', 'angularMoment'])
   .factory('Reminders', ['$http', function ($http) {
-    const obj = [];
-    obj.push($http.get('/api/feedings'));
-    obj.push($http.get('/api/litters'));
-    obj.push($http.get('/api/waterings'));
-    return Promise.all(obj).then(values => {
-      return values;
-    })
+    const reminders = [];
+    reminders.push({ name: 'Feeding', warning: 9, http: $http.get('/api/feedings')});
+    reminders.push({ name: 'Litter', warning: 48, http: $http.get('/api/litters')});
+    reminders.push({ name: 'Plant Watering', warning: 120, http: $http.get('/api/waterings')});
+    return reminders;
   }])
-  .controller('ReminderController', ['$scope', 'Reminders', function ($scope, Reminders) {
-    console.log(Reminders);
-    Reminders
-      .success(function(data) {
-        $scope.reminders = data;
-      })
-      .error(function(data, status){
-        console.log(data, status);
-        $scope.todos = [];
-      });
+  .controller('ReminderController', ['$scope', 'Reminders', 'moment', function ($scope, Reminders, moment) {
+    $scope.reminders = [];
+    Reminders.forEach((reminder) => {
+      reminder.http
+        .success((data) => {
+          $scope.reminders.push({
+            name: reminder.name,
+            warning: getWarningState(moment, data, reminder.warning),
+            elapsed: getTimeSinceDate(moment, data),
+            data: data.map(datedata => new Date(datedata.datetime))
+          });
+        })
+        .error((data, status) => {
+          console.log(data, status);
+          $scope.reminders = [];
+        });
+      console.log($scope.reminders);
+    })
+
   }])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
@@ -26,3 +33,40 @@ angular.module('reminderApp', ['ngRoute', 'ngResource'])
         controller: 'ReminderController'
       });
   }]);
+
+function getTimeSinceDate(moment, data) {
+  if (data[0]) {
+    if (moment().diff(data[0].datetime, 'days')) {
+      return {
+        time: moment().diff(data[0].datetime, 'days'),
+        timename: 'day(s)'
+      };
+    }
+    else if (moment().diff(data[0].datetime, 'hours')) {
+      return {
+        time: moment().diff(data[0].datetime, 'hours'),
+        timename: 'hour(s)'
+      };
+    }
+    else if (moment().diff(data[0].datetime, 'minutes')) {
+      return {
+        time: moment().diff(data[0].datetime, 'minutes'),
+        timename: 'minute(s)'
+      };
+    }
+    else if (moment().diff(data[0].datetime, 'seconds')) {
+      return {
+        time: moment().diff(data[0].datetime, 'seconds'),
+        timename: 'second(s)'
+      };
+    }
+  }
+  return { time: 0, timename: '' };
+}
+
+function getWarningState(moment, data, warning) {
+  if (data[0]) {
+    if (moment().diff(data[0].datetime, 'hours') >= warning) return true;
+  }
+  return false;
+}
